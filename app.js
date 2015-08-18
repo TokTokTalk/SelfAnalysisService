dc0b89c7338f79e4d68a315a3dc9548eb5e5884f
LoopySeo <seogangmin@loopystory.com>
2015-08-18 23:05:04 +0900

    facebook server login test

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express             = require('express')
+    ,app                = express()
+    ,path               = require('path')
+    ,favicon            = require('serve-favicon')
+    ,logger             = require('morgan')
+    ,cookieParser       = require('cookie-parser')
+    ,bodyParser         = require('body-parser')
+    ,session            =require('express-session')
+    ,passport           = require('passport')
+    ,FacebookStrategy   = require('passport-facebook').Strategy
+    ,config             = require('./config/app_config');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+// Use the FacebookStrategy within Passport.
+passport.use(new FacebookStrategy({
+    clientID: config.facebook.api_key,
+    clientSecret:config.facebook.api_secret ,
+    callbackURL: config.facebook.callback_url
+  },
+  function(accessToken, refreshToken, profile, done) {
+
+    process.nextTick(function () {
+      //Check whether the User exists or not using profile.id
+      //Further DB code.
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+      return done(null, profile);
+    });
+
+  }
+));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -20,10 +50,44 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat', key: 'sid'}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//Router code
+app.get('/', function(req, res){
+  //res.render('index', { user: req.user });
+  res.send({ user: req.user });
+});
+app.get('/account', ensureAuthenticated, function(req, res){
+  //res.render('account', { user: req.user });
+  res.send({ user: req.user });
+});
+
+app.get('/fail', function(req, res){
+  res.send({msg:'fail'});
+});
+
+
+//Passport Router
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+       successRedirect : '/',
+       failureRedirect: '/fail'
+  }),
+  function(req, res) {
+    res.redirect('/');
+  });
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/fail')
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +120,6 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.listen(1337);
 
 module.exports = app;
